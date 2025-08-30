979db6f9f00ff1bb49756861a22193e648e0efb5
Sanjana Joseph <164984683+Mathi23ok@users.noreply.github.com>
2025-08-30 10:12:43 +0530

    Update pattern_square.c

--- pattern_square.c
+++ pattern_square.c
@@ -1,8 +1,8 @@
-// Online C compiler to run C program online
+
 #include <stdio.h>
 
 int main() {
-    // Write C code here
+    
     int n;
     printf("Enter the value of n:");
     scanf("%d",&n);
@@ -13,3 +13,14 @@ int main() {
        printf("\n");
     }
 }
+/*
+
+output :
+Enter the value of n:5
+* * * * * 
+* * * * * 
+* * * * * 
+* * * * * 
+* * * * * 
+
+*/
