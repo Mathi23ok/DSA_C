b677e623946baaa9acc9ceb26e29344fcbcc02b1
Sanjana Joseph <164984683+Mathi23ok@users.noreply.github.com>
2025-09-03 05:58:27 +0530

    Create reverse_number.c

---
+++ reverse_number.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter the number :");
+    scanf("%d",&n);
+    printf("Reverse of the number : ");
+    while(n>0){
+        printf("%d",n%10);
+        n = n/10;
+    }
+
+    return 0;
+}
