467fde80a99497a4bcb65712c90fed6ccf32c376
Sanjana Joseph <164984683+Mathi23ok@users.noreply.github.com>
2025-09-06 10:30:05 +0530

    Create pattern_square.c

---
+++ pattern_square.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    
+    int n;
+    printf("Enter the value of n:");
+    scanf("%d",&n);
+    for(int i = 0;i<n;i++){
+       for(int j = 0;j<n;j++){
+           printf("* ");
+       }
+       printf("\n");
+    }
+}
+/*
+
+output :
+Enter the value of n:5
+* * * * * 
+* * * * * 
+* * * * * 
+* * * * * 
+* * * * * 
+
+*/
