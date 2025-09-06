84f2f8600817f239bd44d647f91e1652826f1a0a
Sanjana Joseph <164984683+Mathi23ok@users.noreply.github.com>
2025-09-06 10:39:25 +0530

    Create pattern_num2.c

---
+++ pattern_num2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    
+    int n;
+    printf("Enter the value of n:");
+    scanf("%d",&n);
+    int a = 1;
+    for(int i = 0;i<n;i++){
+       for(int j = 0;j<=i;j++){
+           printf("%d ",a);
+           a++;
+       }
+       printf("\n");
+    }
+}
+/*
+Enter the value of n:5
+1 
+2 3 
+4 5 6 
+7 8 9 10 
+11 12 13 14 15 
+*/
